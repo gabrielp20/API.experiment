157c8333f15303ab3fa9d0770ed3caab568cd2a4
Gabriel Paiva <51544207+gabrielp20@users.noreply.github.com>
2023-02-16 10:58:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,47 +8,54 @@ const conf = require('dotenv').config().parsed; //solicita o modulo "dotenv" par
 const port = conf.HTTPPORT; // obtém a variável de ambiente "HTTPPORT" e armazena o valor em uma variável "port"
 
 // Objeto que será executado quando houver uma requisição.
-const controller = {
- resJson: async (req, res) => {
+const thing = {
+  getAll: async (req, res) => {
+    res.json({ "req": req.method, "status": "ok" });
+  },
+  getOne: async (req, res) => {
+    res.json({ "req": req.method, "id": res.param.id , "status": "ok" });
+  },
+  post: async (req, res) => {
+    res.json({ "req": req.method, "status": "ok" });
+   },
+  put: async (req, res) => { 
+    res.json({ "req": req.method, "status": "ok" });
+  },
+  delete: async (req, res) => {
+    res.json({ "req": req.method, "status": "ok" });
+   }
+}
 
-    // Lista com alguns atributos úteis da requisição (req) HTTP.
-    data = {
-      "method": req.method,
-      "url": req.url,
-      "baseURL": req.baseURL,
-      "query":res.query,
-      "params": req.params,
-      "body": req.body,
-      "headers": req.headers
-    }
-
-    // Envia JSON com os dados acima para o cliente, como texto plano.
-    // res.send(data);
-
-    // Envia JSON com os dados acima para o cliente, como texto plano JSON.
-    res.json(data);
-    
-  }
+// Objeto que trata requisições para o 'user'.
+const user = {
+  getOne: async (req, res) => { },
+  post: async (req, res) => { },
+  put: async (req, res) => { },
+  delete: async (req, res) => { }
 }
 
 // Recebe os dados do body HTTP e valida em JSON.
 const bodyParser = require('body-parser').json();
 
-
 // Rota para GET → getAll() → Recebe, por exemplo, todos os registros.
-app.get('/', controller.resJson);
+app.get('/', thing.getAll);
 
 // Rota para GET → get(id) → Recebe apenas o registro identificado.
-app.get('/:id', controller.resJson);
+app.get('/:id', thing.getOne);
 
 // Rota para DELETE.
-app.delete('/:id', controller.resJson);
+app.delete('/:id', thing.delete);
 
 // Rota para POST. => bodyPartser e usado para garantir a chegada de um JSON
-app.post('/', bodyParser, controller.resJson);
+app.post('/', bodyParser, thing.post);
 
 // Rota para PUT.
-app.put('/', controller.resJson);
+app.put('/:id', bodyParser, thing.put);
+
+// Rotas para o usuário
+app.get('/user/:id', user.getOne);
+app.put('/user/:id', user.put);
+app.delete('/user/:id', user.delete);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
