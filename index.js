d560121f29032b840990dcfbbf80c997cc11604d
Gabriel Paiva <51544207+gabrielp20@users.noreply.github.com>
2023-02-17 11:52:30 -0300

    att

--- index.js
+++ index.js
@@ -1,30 +1,133 @@
 const express = require('express') // solicita o modulo "express" para o Node.js
 const app = express() // instancia um objeto "app" com a função "express"
-
 const conf = require('dotenv').config().parsed; //solicita o modulo "dotenv" para obter variáveis de ambiente e armazenar como objeto
 
 // console.log(conf);
+
+// Importa a biblioteca "MySQL".
+const mysql = require('mysql2');
+
+// Faz a conexão com o servidor.
+const conn = mysql.createPool({
+  host: conf.HOSTNAME,
+  user: conf.USERNAME,
+  database: conf.DATABASE,
+  password: conf.PASSWORD,
+  port: conf.HOSTPORT
+}).promise();
 
 const port = conf.HTTPPORT; // obtém a variável de ambiente "HTTPPORT" e armazena o valor em uma variável "port"
 
 // Objeto que será executado quando houver uma requisição.
 const thing = {
   getAll: async (req, res) => {
-    res.json({ "req": req.method, "status": "ok" });
+
+    try {
+      // Query que obtém os dados do banco de dados.
+      const sql = "SELECT *, DATE_FORMAT(tdate, '%d/%m/%Y às %H:%i') AS tdatebr FROM things WHERE tstatus = 'on' ORDER BY tdate DESC";
+
+      const [rows] = await conn.query(sql);
+
+      // Views dos dados
+      res.json({ data: rows });
+    }
+
+    catch (error) {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+
+    }
   },
+
   getOne: async (req, res) => {
-    res.json({ "req": req.method, "id": res.param.id , "status": "ok" });
+
+    try {
+
+      // Id da requisição
+      const id = req.params.id;
+
+      const sql = "SELECT *, DATE_FORMAT(tdate, '%d/%m/%Y às %H:%i') AS tdatebr FROM things WHERE tid = ? tstatus = 'on' ORDER BY tdate DESC";
+      const [rows] = await conn.query(sql, [id]);
+
+      // Views dos dados
+      res.json({ coisa: rows });
+
+    } catch (error) {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+
+    }
   },
+
   post: async (req, res) => {
-    res.json({ "req": req.method, "status": "ok" });
-   },
-  put: async (req, res) => { 
-    res.json({ "req": req.method, "status": "ok" });
+
+    try {
+
+      // Extrai os campos do req.body.
+      const { user, name, photo, description, location, options } = req.body;
+
+      // Query
+      const sql = "INSERT INTO things (tuser, tname, tphoto , tdescription, tlocation, toptions) VALUES (?, ?, ?, ?, ?, ?)";
+      const [rows] = await conn.query(sql, [user, name, photo, description, location, options]);
+
+       // View → feedback.
+       res.json({ id: rows.insertId, status: "success" });
+
+    } catch {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+
+    }
   },
+
+  put: async (req, res) => {
+
+    try {
+
+      // Id da requisição
+      const id = req.params.id;
+
+      // Extrai os campos do req.body.
+      const { user, name, photo, description, location, options } = req.body;
+
+      // Query
+      const sql = "UPDATE things SET tuser = ?, tname = ?, tphoto = ?, tdescription = ?, tlocation = ?, toptions = ? WHERE tid = ?";
+      const [rows] = await conn.query(sql, [user, name, photo, description, location, options, id]);
+
+      // View → feedback.
+      res.json({ status: "success" });
+
+    } catch {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+
+    }
+  },
+
+
   delete: async (req, res) => {
-    res.json({ "req": req.method, "status": "ok" });
-   }
-}
+    try {
+
+      // Id da requisição
+      const id = req.params.id;
+
+      // Query
+      const sql = "UPDATE things SET tstatus = 'off' WHERE tid = ?";
+      const [rows] = await conn.query(sql, [id]);
+
+      // View 
+      res.json({ status: "success" });
+
+    } catch {
+
+      // Exibe mensagem de erro.
+      res.json({ status: "error", message: error });
+  }
+}}
 
 // Objeto que trata requisições para o 'user'.
 const user = {
